55cdfe6a1a1f798e34ad4341e45fbc1605f748cc
josscg449 <38119088+josscg449@users.noreply.github.com>
2020-05-16 15:16:20 -0600

    Updates

---
+++ precache-manifest.51451d7eea92cd2ef5b9af0924aa98b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b13a516bf0fc31f5132df8ef42d2e7",
+    "url": "/markdown/index.html"
+  },
+  {
+    "revision": "586efb30cf300219b349",
+    "url": "/markdown/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67f0ba48ea5098cba9ec",
+    "url": "/markdown/static/js/2.c6da592c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown/static/js/2.c6da592c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586efb30cf300219b349",
+    "url": "/markdown/static/js/main.650d2f82.chunk.js"
+  },
+  {
+    "revision": "d0dc84686c7ca4d6116b",
+    "url": "/markdown/static/js/runtime-main.fa656a0e.js"
+  }
+]);
